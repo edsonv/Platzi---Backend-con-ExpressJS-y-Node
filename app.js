e64d64c76e83d76a386e53ae6001aed9b6d8d5a9
Edson J Vargas <edson.jhair@yahoo.com>
2025-06-06 11:38:50 -0500

    Clase 3

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send(`
+    <h1>Curso Express.js</h1>
+    <p>Esto es una aplicación node.js con express.js</p>
+    <p>Corre en el puerto: ${PORT}</p>
+    `);
 });
 
 app.listen(PORT, () => {
-  console.log('Nuestra aplicacion está funcionando');
+  console.log('Server is running on port', PORT);
+  console.log('Visit http://localhost:' + PORT);
 });
